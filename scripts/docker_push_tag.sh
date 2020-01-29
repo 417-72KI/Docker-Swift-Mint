954420dbe333d9b479f125e585bc1aaf2deff324
417-72KI <417.72ki@gmail.com>
2020-01-29 15:59:25 +0900

    Use bash

--- scripts/docker_push_tag.sh
+++ scripts/docker_push_tag.sh
@@ -1,13 +1,4 @@
-#!/bin/zsh
-
-local -A opthash
-zparseopts -D -A opthash -- -force f
-
-if [[ -n "$opthash[(i)--force]" ]]; then
-    FORCE_MODE=true
-else
-    FORCE_MODE=false
-fi
+#!/bin/bash
 
 if [ $# -ne 3 ]; then
     echo "Usage: $0 docker_image repository version"
@@ -21,11 +12,6 @@ TAG=$3
 if ! docker images -q | grep $DOCKER_IMAGE > /dev/null; then
     echo "Image($DOCKER_IMAGE) not exists."
     exit 1
-fi
-
-if [ $(`dirname $0`/docker_tag_exists.sh $REPOSITORY $TAG) = true ] && [ $FORCE_MODE = false ]; then
-    echo "$REPOSITORY:$TAG already exists. skip"
-    exit 0
 fi
 
 docker tag $DOCKER_IMAGE "$REPOSITORY:$TAG"
