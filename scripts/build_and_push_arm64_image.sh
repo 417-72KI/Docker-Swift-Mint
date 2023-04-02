fdbb428ea7860fa187b36b5a41e1bc07216db659
417-72KI <417.72ki@gmail.com>
2023-04-03 02:39:01 +0900

    create script

---
+++ scripts/build_and_push_arm64_image.sh
@@ -0,0 +1,41 @@
+#!/bin/zsh
+
+if [[ $(arch) != "arm64" ]]; then
+    echo "\e[31mThis script must be run on an arm64 machine.\e[m"
+    exit 1
+fi
+
+DOCKER_USER=$1
+IMAGE_NAME=$2
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+for version in $(cat $REPO_ROOT/.github/matrix.json | jq -r '.swift_version[]'); do
+    # Less than 5.6 doesn't support arm64
+    if [[ $(($version)) -lt 5.6 ]]; then
+        continue
+    fi
+
+    docker manifest inspect $DOCKER_USER/$IMAGE_NAME:$version > /dev/null 2>/dev/null
+    if [[ $? -ne 0 ]]; then
+        # Pull amd64 image and re-tag
+        docker pull --platform=linux/amd64 $DOCKER_USER/$IMAGE_NAME:$version
+        AMD64_IMAGE_ID=$(docker images --format "{{.Repository}}:{{.Tag}} {{.ID}}" | grep $DOCKER_USER/$IMAGE_NAME:$version | grep -v arm64 | awk '{print $2}')
+        docker tag $AMD64_IMAGE_ID $DOCKER_USER/$IMAGE_NAME:$version-amd64
+        docker push $DOCKER_USER/$IMAGE_NAME:$version-amd64
+
+        # Build arm64 image
+        docker build --build-arg SWIFT_VERSION=$version -t $DOCKER_USER/$IMAGE_NAME:$version-arm64 -f $REPO_ROOT/Dockerfile $REPO_ROOT
+        docker push $DOCKER_USER/$IMAGE_NAME:$version-arm64
+
+        # Create manifest
+        docker manifest create $DOCKER_USER/$IMAGE_NAME:$version \
+            $DOCKER_USER/$IMAGE_NAME:$version-amd64 \
+            $DOCKER_USER/$IMAGE_NAME:$version-arm64
+        docker manifest push $DOCKER_USER/$IMAGE_NAME:$version
+
+        # Clean up
+        docker image rm $DOCKER_USER/$IMAGE_NAME:$version \
+            $DOCKER_USER/$IMAGE_NAME:$version-amd64 \
+            $DOCKER_USER/$IMAGE_NAME:$version-arm64
+    fi
+done
