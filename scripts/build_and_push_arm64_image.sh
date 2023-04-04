8cf6fdc9cdc1d386e9c6035364797ab7d16fbc34
417-72KI <417.72ki@gmail.com>
2023-04-04 11:51:23 +0900

    add log

--- scripts/build_and_push_arm64_image.sh
+++ scripts/build_and_push_arm64_image.sh
@@ -22,20 +22,27 @@ for version in $(echo "$MATRIX_JSON" | jq -r '.swift_version[]'); do
     docker manifest inspect "$DOCKER_USER/$IMAGE_NAME:$version" > /dev/null 2>/dev/null
     if [[ $? -ne 0 ]]; then
         # Pull amd64 image and re-tag
+        echo "\e[32mFetching amd64 image started...\e[m"
         docker pull --platform=linux/amd64 "$DOCKER_USER/$IMAGE_NAME:$version"
+        echo "\e[32mFetching amd64 image finished!\e[m"
         AMD64_IMAGE_ID=$(docker images --format "{{.Repository}}:{{.Tag}} {{.ID}}" | grep "$DOCKER_USER/$IMAGE_NAME:$version" | grep -v arm64 | awk '{print $2}')
         docker tag $AMD64_IMAGE_ID "$DOCKER_USER/$IMAGE_NAME:$version-amd64"
         docker push "$DOCKER_USER/$IMAGE_NAME:$version-amd64"
+        echo "\e[32mRe-tagging amd64 image finished!\e[m"
 
         # Build arm64 image
+        echo "\e[32mBuilding arm64 image started...\e[m"
         docker build --build-arg SWIFT_VERSION=$version -t "$DOCKER_USER/$IMAGE_NAME:$version-arm64" -f "$REPO_ROOT/Dockerfile" "$REPO_ROOT"
         docker push "$DOCKER_USER/$IMAGE_NAME:$version-arm64"
+        echo "\e[32mBuilding arm64 image finished!\e[m"
 
         # Create manifest
+        echo "\e[32mCreating manifest started...\e[m"
         docker manifest create "$DOCKER_USER/$IMAGE_NAME:$version" \
             --amend "$DOCKER_USER/$IMAGE_NAME:$version-amd64" \
             --amend "$DOCKER_USER/$IMAGE_NAME:$version-arm64"
         docker manifest push "$DOCKER_USER/$IMAGE_NAME:$version"
+        echo "\e[32mCreating manifest finished!\e[m"
 
         # Create latest manifest
         if [[ "$version" == "$LATEST_VERSION" ]]; then
